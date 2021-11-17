c790368be3eb64233ab2f8f9147a06a2a010ec37
Ivan <ivanxintang@gmail.com>
2021-11-17 13:52:43 -0800

    Refactor composer-char-counter to use addClass instead of changing css directly

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,15 @@
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
-    let maxLength = 140;
-    let $length = $(this).val().length;
+    const maxLength = 140;
+    const $length = $(this).val().length;
     const $counter = $(this).parent().find('output');
+
     $counter.val(maxLength - $length);
+
     if ($length > maxLength) {
-      $counter.css('color', 'red');
+      return $counter.addClass("overLimit");
     }
+    $counter.removeClass("overLimit");
   });
 });
