798a77a9e5c37df40757d4e876a62a92110032e8
Ivan <ivanxintang@gmail.com>
2021-11-17 16:13:22 -0800

    Implement renderTweets(), createTweetElement(), loadTweets(), escape(), and form submission ajax posts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,80 @@
+/* eslint-disable no-undef */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(tweet => {
+      $('#all-tweets').prepend(createTweetElement(tweet));
+    });
+  };
+
+  const createTweetElement = (data) => {
+    const $tweet = $(`
+    <article class="tweet">
+    <header>
+      <span class="tweet-avatar">
+        <img src="${data.user.avatars}" alt="user avatar">
+        <p>${data.user.name}</p>
+      </span>
+      <p class="username"><strong>${data.user.handle}</strong></p>
+    </header>
+    <p>
+      <strong>${escape(data.content.text)}</strong>
+    </p>
+    <footer>
+      <p>${timeago.format(data.created_at)}</p>
+      <span class="social-buttons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
+    </article>
+    `);
+    return $tweet;
+  };
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    const maxLength = 140;
+    const tweetBody = data.substr(5);
+    if (tweetBody.length > maxLength) {
+      return alert("Your tweet is too long!");
+    }
+    if (!tweetBody) {
+      return alert("You didn't write anything!");
+    }
+
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      type: "POST",
+      data
+    }).then(() => {
+      $(this)[0].reset();
+      loadTweets();
+    });
+  });
+
+  const loadTweets = () => {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+    })
+      .then((tweets) => {
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
+
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+});
