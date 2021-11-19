9b3eb91520a041e610a6c944c662dd615ef34557
Ivan <ivanxintang@gmail.com>
2021-11-18 20:20:37 -0800

    Add brief comments to explain functionality and remove console logs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,18 @@
  */
 
 $(document).ready(function() {
+  // character limit for our textarea
+  const maxCharLength = 140;
+
+  // set counter to the maximum character limit
+  $('.counter').val(maxCharLength);
+  // hide error message until it's needed
   $('#error').hide();
+
+  /**
+   * renderTweets takes in a tweets database and
+   * prepends each tweet to the all-tweets container
+   */
   const renderTweets = function(tweets) {
     tweets.forEach(tweet => {
       console.log(tweet);
@@ -14,6 +25,7 @@ $(document).ready(function() {
     });
   };
 
+  // createTweetElement takes in a data object with tweet information and returns a formatted HTML tweet
   const createTweetElement = (data) => {
     const $tweet = $(`
     <article class="tweet">
@@ -40,36 +52,49 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  // handle form submission for new tweets
   $("form").on("submit", function(event) {
     event.preventDefault();
-    console.log("hi");
     const data = $(this).serialize();
-    const maxLength = 140;
-    const tweetBody = data.substr(5);
-    if (tweetBody.length > maxLength) {
+    const tweetBody = data.substr(5).trim();
+
+    // return error if the text in body is > maxCharLength
+    if (tweetBody.length > maxCharLength) {
       $("#error p").text("You wrote too much!");
       return $("#error").slideDown("fast");
     }
-    if (!tweetBody) {
+
+    // return error if text in body is empty
+    if (!tweetBody.trim() || !tweetBody.length) {
       $("#error p").text("You didn't write anything!");
       return $("#error").slideDown("fast");
     }
 
+    // ajax post request to post a tweet
     $.ajax({
       url: "http://localhost:8080/tweets",
       type: "POST",
       data
     }).then(() => {
+      // slide error message back up if post request resolves
       if ($("#error p").is(":visible")) {
         $("#error").slideUp("fast");
       }
+
+      /**
+       * reset the form after successful form submission
+       * reset the counter for character limit
+       * empty current tweets for rendering new list of tweets
+       * render all tweets in database
+       */
       $(this)[0].reset();
+      $('.counter').val(maxCharLength);
       $('.all-tweets').empty();
       loadTweets();
     });
   });
 
-
+  // loadTweets fetches tweets from the db and passes it into renderTweets.
   const loadTweets = () => {
     $.ajax({
       url: "http://localhost:8080/tweets",
@@ -82,6 +107,7 @@ $(document).ready(function() {
 
   loadTweets();
 
+  // escape function to prevent XSS
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
