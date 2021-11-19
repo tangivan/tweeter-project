1c420427b94c63079fc3561e1aada70c21edd884
Ivan <ivanxintang@gmail.com>
2021-11-18 20:25:31 -0800

    Change scrollButton function to use jquery to find scroll position instead of javascript, and add brief comments to make code more clear

--- public/scripts/scroll-to-top.js
+++ public/scripts/scroll-to-top.js
@@ -1,18 +1,27 @@
 /* eslint-disable no-undef */
 $(document).ready(function() {
+  // bind scroll to the window element and call scrollButton when scroll detected
   $(window).scroll(function() {
     scrollButton();
   });
 
+  // go back to top of page with smooth scroll animation
   $(".up-button").on("click", function() {
     $("html, body").animate({
       scrollTop: 0
     }, "slow");
+
   });
+
+  // check if scroll position is 150 pixels off from the top and sets display accordingly
   const scrollButton = () => {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    // get scroll position;
+    const scrollPosition = $(window).scrollTop();
+    if (scrollPosition > 150) {
+      // show the button
       $(".up-button")[0].style.display = "block";
     } else {
+      // hide the button
       $(".up-button")[0].style.display = "none";
     }
   };
