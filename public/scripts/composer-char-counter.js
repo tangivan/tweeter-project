2f93f9d047f81c3afa3e4ecc1cf1bcf065625030
Ivan <ivanxintang@gmail.com>
2021-11-18 20:23:36 -0800

    Add brief comments to make code more clear

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 $(document).ready(function() {
+  // checks if current character length is over the maximum character length
   $("#tweet-text").on('input', function() {
-    const maxLength = 140;
     const $length = $(this).val().length;
     const $counter = $(this).parent().find('output');
+    const maxLength = 140;
 
     $counter.val(maxLength - $length);
 
     if ($length > maxLength) {
-      return $counter.addClass("overLimit");
+      // change counter color to red if over limit
+      return $counter.addClass("js-overLimit");
     }
-    $counter.removeClass("overLimit");
+    // removes the red color if it current length is not over limit
+    $counter.removeClass("js-overLimit");
   });
 });
